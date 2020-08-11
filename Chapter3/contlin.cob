a62605b2a6875a35fbc00af307857a64c34acc28
GroupTheorist12 <55472994+GroupTheorist12@users.noreply.github.com>
2020-08-11 00:36:46 -0400

    new contlin

---
+++ Chapter3/contlin.cob
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.     'CONTLIN'.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+         WORKING-STORAGE SECTION.
+         01 WS-SQL  PIC X(200) VALUE  'select * from cars
+      -                               ' where id > 3000'.
+
+       PROCEDURE DIVISION.
+              DISPLAY 'WS-NAME: ' WS-SQL UPON  CONSOLE.
+           STOP RUN.
