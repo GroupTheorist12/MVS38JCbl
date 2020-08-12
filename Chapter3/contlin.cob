58f523d003d468bd3aed3a9c94fb25d1b14e974a
GroupTheorist12 <55472994+GroupTheorist12@users.noreply.github.com>
2020-08-12 00:03:23 -0400

    new WS-SQL added

--- Chapter3/contlin.cob
+++ Chapter3/contlin.cob
@@ -3,9 +3,9 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
          WORKING-STORAGE SECTION.
-         01 WS-SQL  PIC X(200) VALUE  'select * from cars
-      -                               ' where id > 3000'.
+         01 WS-SQL  PIC X(200) VALUE  'select * from cars where id > 
+      -                               '3000'.
 
        PROCEDURE DIVISION.
-              DISPLAY 'WS-NAME: ' WS-SQL UPON  CONSOLE.
+              DISPLAY 'WS-SQL: ' WS-SQL UPON  CONSOLE.
            STOP RUN.
